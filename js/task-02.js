f7e3b1e7e9a3897674aec4f50cd95d0daf1f260a
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-09-09 15:16:38 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsUlRef=document.querySelector("#ingredients")
+const listItem=ingredients.map((ingredient)=>{
+  const itemLi=document.createElement("li")
+  itemLi.textContent=ingredient;
+  itemLi.classList.add("item");
+  return itemLi;
+})
+
+ingredientsUlRef.append(...listItem);
