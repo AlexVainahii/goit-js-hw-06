11b67975dfc18a99d2206e13b8a3cc933330018b
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-09-09 21:51:53 +0300

    sum

--- js/task-04.js
+++ js/task-04.js
@@ -1,10 +1,7 @@
 let counterValue = 0;
 const counter = document.querySelector("#value");
-const buttonInc = document.querySelector("*[data-action=increment]");
-console.log(buttonInc);
-const buttonDec = document.querySelector("*[data-action=decrement]");
-console.log(buttonDec);
-
+const buttonInc = document.querySelector('button[data-action="increment"]');
+const buttonDec = document.querySelector('button[data-action="decrement"]');
 buttonInc.addEventListener("click", () => {
   counterValue += 1;
   counter.textContent = counterValue;
---
+++ js/task-05.js
@@ -0,0 +1,5 @@
+const nameInputRef = document.querySelector("#name-input");
+const nameOutputRef = document.querySelector("#name-output");
+nameInputRef.addEventListener("input", () => {
+    if nameInputRef.chan
+})
