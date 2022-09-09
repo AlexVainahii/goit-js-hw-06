a2b0050aa74c9a5eb888d0ba64b04f9d7373ca87
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-09-09 22:32:53 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,5 @@
 const inputValue = document.querySelector("#font-size-control");
 const textSpan = document.querySelector("#text");
-
 inputValue.addEventListener("input", (event) => {
-  console.log(event.currentTarget.value);
   textSpan.style.fontSize = event.currentTarget.value + "px";
 });
