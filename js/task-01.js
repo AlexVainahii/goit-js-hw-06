45583b30f76cee49761ef7a53a892c860a75cd53
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-09-10 00:25:24 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
-const categoriesMainRef = document.querySelector("#categories");
-const categoriesRef = categoriesMainRef.querySelectorAll("ul");
-console.log("Number of categories:", categoriesRef.length);
-const categoriesListRef = categoriesMainRef.querySelectorAll("li.item");
-categoriesListRef.forEach((element) => {
+const categoriesMain = document.querySelector("#categories");
+const categories = categoriesMain.querySelectorAll("ul");
+console.log("Number of categories:", categories.length);
+const categoriesList = categoriesMain.querySelectorAll("li.item");
+categoriesList.forEach((element) => {
   console.log("Category:", element.querySelector("h2").textContent);
   console.log("Elements:", element.querySelector("ul").children.length);
 });
