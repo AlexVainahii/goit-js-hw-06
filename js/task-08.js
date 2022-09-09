7fb1cc688809a46bf320113978013dff40d51005
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-09-09 23:53:01 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,2 +1,15 @@
-const loginEmail = document.querySelector("form.login-form");
-console.log(loginEmail);
+const elemForm = document.querySelector("form.login-form");
+
+elemForm.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
